c0c72e7248964b47c8e2f809ce1849d0cdf597d2
Manwoo Yi <navilera@gmail.com>
2019-03-09 03:43:16 -0800

    Add eFlash page writting function

---
+++ app/bluepill/Flash.c
@@ -0,0 +1,34 @@
+/*
+ * Flash.c
+ *
+ *  Created on: Mar 9, 2019
+ *      Author: maanu
+ */
+
+#include "stdint.h"
+#include "MemoryMap.h"
+
+#include "stm32f1xx_hal_flash.h"
+
+extern void FLASH_PageErase(uint32_t PageAddress);
+extern HAL_StatusTypeDef HAL_FLASH_Program(uint32_t TypeProgram, uint32_t Address, uint32_t Data);
+
+void Flash_write_page(uint8_t* pageBuffer, uint32_t PageAddress)
+{
+	uint32_t len = EFLASH_PAGESIZE / 4;
+	uint32_t addr = EFLASH_BADDR(PageAddress);
+
+	HAL_FLASH_Unlock();
+
+	FLASH_PageErase(addr);
+
+	uint32_t* pData = (uint32_t*)pageBuffer;
+
+	while(len--)
+	{
+		HAL_FLASH_Program(2, addr, *pData++);
+		addr += 4;
+	}
+
+	HAL_FLASH_Lock();
+}
---
+++ app/bluepill/Flash.h
@@ -0,0 +1,15 @@
+/*
+ * Flash.h
+ *
+ *  Created on: Mar 9, 2019
+ *      Author: maanu
+ */
+
+#ifndef APP_BLUEPILL_FLASH_H_
+#define APP_BLUEPILL_FLASH_H_
+
+#include "stdint.h"
+
+void Flash_write_page(uint8_t* pageBuffer, uint32_t PageAddress);
+
+#endif /* APP_BLUEPILL_FLASH_H_ */
