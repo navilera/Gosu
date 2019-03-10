92168604d5e86958f7c995b4ed7bbd1e56cd427a
Manwoo Yi <navilera@gmail.com>
2019-03-10 00:36:55 -0800

    Disable external LED

--- hal/bluepill/Gpio.c
+++ hal/bluepill/Gpio.c
@@ -10,12 +10,13 @@
 
 void Hal_gpio_init(void)
 {
-	GPIO_InitTypeDef GPIO_InitStruct;
-
 	/* GPIO Ports Clock Enable */
 	__HAL_RCC_GPIOA_CLK_ENABLE();
 	__HAL_RCC_GPIOB_CLK_ENABLE();
 	__HAL_RCC_GPIOC_CLK_ENABLE();
+
+#if 0	// actually, we don't need this indication LED. Disable temporary and it will be removed in the future.
+	GPIO_InitTypeDef GPIO_InitStruct;
 
 	/*Configure GPIO pin Output Level */
 	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
@@ -25,5 +26,6 @@ void Hal_gpio_init(void)
 	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
 	HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+#endif
 }
 
