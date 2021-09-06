787109a1b5bb3c7163fac236a03efd3d49c8242c
Manwoo Yi <navilera@gmail.com>
2021-09-06 00:33:01 -0700

    Improvement : auto-repeat key holding press

--- app/PollingTask.c
+++ app/PollingTask.c
@@ -15,6 +15,8 @@
 
 #include "Kernel.h"
 
+#define HID_KEY_INTERVAL_100MS		(10)		// 10 times per 1sec -> 100ms
+#define DELAY_REPEAT_COUNT			(50)		// 30 count delay
 
 void Polling_task(void)
 {
@@ -22,6 +24,7 @@ void Polling_task(void)
 
     bool pressedFnKey = false;
     uint32_t pollingCount = 0;
+	uint32_t repeatDelayCount = 0;
 
     KeyHwAddr_t hwPollingAddrs[HID_MAX_MULTIPLE_INPUT];
 
@@ -32,7 +35,7 @@ void Polling_task(void)
 
     while (true)
     {
-    	USBD_Delay(HID_FS_BINTERVAL);
+    	USBD_Delay(HID_KEY_INTERVAL_100MS);
 
     	memclr(hwPollingAddrs, HID_MAX_MULTIPLE_INPUT * sizeof(KeyHwAddr_t));
     	memclr(HIDKeyboardReport, HID_KBD_REPORT_BYTE);
@@ -42,13 +45,33 @@ void Polling_task(void)
     	pressedFnKey = KeyMap_checkFnKey(hwPollingAddrs, pollingCount);
     	KeyMap_getReport(pressedFnKey, HIDKeyboardReport, hwPollingAddrs, pollingCount);
 
-		if (memncmp(HIDKeyboardReportOld, HIDKeyboardReport, HID_KBD_REPORT_BYTE) == false)
+		if (memncmp(HIDKeyboardReportOld, HIDKeyboardReport, HID_KBD_REPORT_BYTE))
 		{
-			Kernel_send_msg(KernelMsgQ_D2hData, HIDKeyboardReport, HID_KBD_REPORT_BYTE);
-			Kernel_send_events(KernelEventFlag_SendD2H);
-
-			memncpy(HIDKeyboardReportOld, HIDKeyboardReport, HID_KBD_REPORT_BYTE);
+			if (HIDKeyboardReport[HID_MODIKEY_IDX] == 0x00 && HIDKeyboardReport[HID_KEY_START_IDX] == 0x00)
+			{	// no input
+				repeatDelayCount = 0;
+				Kernel_yield();
+				continue;
+			}
+			else
+			{	// repeat any key
+				// 500ms delay before repeat
+				repeatDelayCount++;
+				if (repeatDelayCount < DELAY_REPEAT_COUNT)
+				{
+					continue;
+				}
+			}
 		}
+		else
+		{
+			repeatDelayCount = 0;
+		}
+
+		Kernel_send_msg(KernelMsgQ_D2hData, HIDKeyboardReport, HID_KBD_REPORT_BYTE);
+		Kernel_send_events(KernelEventFlag_SendD2H);
+
+		memncpy(HIDKeyboardReportOld, HIDKeyboardReport, HID_KBD_REPORT_BYTE);
 
         Kernel_yield();
     }
